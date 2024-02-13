1254b59aa5d2dd1a0e45b2daf8db772a75165353
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-13 15:13:49 +0000

    added HelloWorldWithStyle.swift

---
+++ HelloWorldWithStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Jessah
+//  Created on 2024-02-09
+//  Version 1.0
+//  Copyright (c) 2024 JessahT. All rights reserved.
+//
+//  Print statement to test linter
+
+
+print("Hello World, but with style!!!")
